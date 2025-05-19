6201467cd4a686e629b95c172d9a7bab982a9135
Aishwaryasai04 <aishwaryasai44@gmail.com>
2025-05-18 22:08:23 -0500

    Create valid_anagram.py

---
+++ valid_anagram.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        r=s.split()
+        for i in range(r):
+            if r.char==t:
+                return true
